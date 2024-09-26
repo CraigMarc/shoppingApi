547b985e62de314ea9d706d92d258ec539ecb4f4
craigmarc <cmarcinkiewicz2000@yahoo.com>
2024-09-26 17:35:26 -0400

    added product schema

---
+++ models/inventory.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const ProductSchema = new Schema({
+  title: { type: String, required: true, minLength: 1 },
+  category: { type: String, required: true, minLength: 1 },
+  brand: { type: String, minLength: 1, },
+  color: { type: String, minLength: 1 },
+  description: { type: String, required: true, minLength: 1 },
+  modelNum: { type: String, minLength: 1 },
+  price: { type: Number, required: true, minLength: 1 },
+  quantity: { type: Number, required: true, minLength: 1 },
+  timestamp: { type: Date, default: new Date() },
+  published: { type: Boolean },
+  image: { type: String }, 
+});
+
+
+
+
+// Export model
+module.exports = mongoose.model("Posts", PostsSchema);
