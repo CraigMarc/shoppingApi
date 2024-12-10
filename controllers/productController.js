94466d9d68e2b500ee76d74ef7b8a09f2d021742
craigmarc <cmarcinkiewicz2000@yahoo.com>
2024-12-10 15:31:43 -0500

    resized images when uploaded

--- controllers/productController.js
+++ controllers/productController.js
@@ -4,6 +4,8 @@ const { body, validationResult } = require("express-validator")
 const mongoose = require("mongoose");
 const Product = require("../models/inventory");
 const fs = require('fs');
+const sharp = require('sharp');
+
 
 // set up multer for pic uploads
 
@@ -17,6 +19,8 @@ let storage = multer.diskStorage({
     cb(null, file.fieldname + '-' + Date.now() + '.' + extension)
   }
 })
+
+
 
 // only allow jpeg and png files
 
@@ -34,147 +38,182 @@ const upload = multer({
   fileFilter: fileFilter
 });
 
+
+
+// use multer and sharp to resize pics
+
+// memory storage so pic can be resized
+
+const memstorage = multer.memoryStorage();
+
+const filter = (req, file, cb) => {
+  if (file.mimetype.split("/")[0] === 'image') {
+    cb(null, true);
+  } else {
+    cb(new Error("Only images are allowed!"));
+  }
+};
+
+imageUploader = multer({
+  memstorage,
+  fileFilter: filter
+});
+
 // post new product
 
-    exports.post_product = [
+exports.post_product = [
 
-      // Handle single file upload with field name "image"
-      upload.single("image"),
-    
-      body("title")
-        .trim()
-        .isLength({ min: 1 })
-        .escape()
-        .withMessage("title must be specified."),
-      body("category")
-        .trim()
-        .isLength({ min: 1 })
-        .escape()
-        .withMessage("category must be specified."),
-      body("brand")
-        .trim()
-        .escape(),
-      body("color")
-        .trim()
-        .escape(),
-      body("brand")
-        .trim()
-        .escape(),
-      body("description")
-        .trim()
-        .isLength({ min: 1 })
-        .escape()
-        .withMessage("description must be specified."),
-      body("modelNum")
-        .trim()
-        .escape(),
-      body("price")
-        .trim()
-        .isLength({ min: 1 })
-        .isNumeric()
-        .escape()
-        .withMessage("price must be specified."),
-      body("quantity")
-        .trim()
-        .isLength({ min: 1 })
-        .isNumeric()
-        .escape()
-        .withMessage("quantity must be specified."),
-      body("length")
-        .trim()
-        .isLength({ min: 1 })
-        .isNumeric()
-        .escape()
-        .withMessage("length must be specified."),
-      body("width")
-        .trim()
-        .isLength({ min: 1 })
-        .isNumeric()
-        .escape()
-        .withMessage("width must be specified."),
-      body("height")
-        .trim()
-        .isLength({ min: 1 })
-        .isNumeric()
-        .escape()
-        .withMessage("height must be specified."),
-      body("weight")
-        .trim()
-        .isLength({ min: 1 })
-        .isNumeric()
-        .escape()
-        .withMessage("weight must be specified."),
-       
-    
-      async function (req, res, next) {
-    
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-          
-          res.json({
-            data: req.body,
-            errors: errors.array(),
-          });
-          return;
-        }
-    
-    
-        if (req.file) {
-          const product = new Product({
-            title: req.body.title,
-            category: req.body.category,
-            brand: req.body.brand,
-            color: req.body.color,
-            description: req.body.description,
-            modelNum: req.body.modelNum,
-            price: req.body.price,
-            length: req.body.length,
-            width: req.body.width,
-            height: req.body.height,
-            weight: req.body.weight,
-            quantity: req.body.quantity,
-            published: false,
-            image: req.file.filename
-          });
-          try {
-            await product.save()
-            let allProducts = await Product.find().exec()
-            res.status(200).json(allProducts)
-          } catch (error) {
-            res.status(500).json({ message: error });
-          }
-        }
-        else {
-          const product = new Product({
-            title: req.body.title,
-            category: req.body.category,
-            brand: req.body.brand,
-            color: req.body.color,
-            description: req.body.description,
-            modelNum: req.body.modelNum,
-            price: req.body.price,
-            length: req.body.length,
-            width: req.body.width,
-            height: req.body.height,
-            weight: req.body.weight,
-            quantity: req.body.quantity,
-            published: false
-    
-          });
-          try {
-            await product.save()
-            let allProducts = await Product.find().exec()
-            res.status(200).json(allProducts)
-          } catch (error) {
-            res.status(500).json({ message: error });
-          }
-        }
-    
-    
+  // Handle single file upload with field name "image"
+  // upload.single("image"),
+  imageUploader.single('image'),
+
+  body("title")
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage("title must be specified."),
+  body("category")
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage("category must be specified."),
+  body("brand")
+    .trim()
+    .escape(),
+  body("color")
+    .trim()
+    .escape(),
+  body("brand")
+    .trim()
+    .escape(),
+  body("description")
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage("description must be specified."),
+  body("modelNum")
+    .trim()
+    .escape(),
+  body("price")
+    .trim()
+    .isLength({ min: 1 })
+    .isNumeric()
+    .escape()
+    .withMessage("price must be specified."),
+  body("quantity")
+    .trim()
+    .isLength({ min: 1 })
+    .isNumeric()
+    .escape()
+    .withMessage("quantity must be specified."),
+  body("length")
+    .trim()
+    .isLength({ min: 1 })
+    .isNumeric()
+    .escape()
+    .withMessage("length must be specified."),
+  body("width")
+    .trim()
+    .isLength({ min: 1 })
+    .isNumeric()
+    .escape()
+    .withMessage("width must be specified."),
+  body("height")
+    .trim()
+    .isLength({ min: 1 })
+    .isNumeric()
+    .escape()
+    .withMessage("height must be specified."),
+  body("weight")
+    .trim()
+    .isLength({ min: 1 })
+    .isNumeric()
+    .escape()
+    .withMessage("weight must be specified."),
+
+
+  async function (req, res, next) {
+
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.json({
+        data: req.body,
+        errors: errors.array(),
+      });
+      return;
+    }
+
+
+    if (req.file) {
+
+       // path: where to store resized photo
+       let extArray = req.file.mimetype.split("/");
+       let extension = extArray[extArray.length - 1];
+       const path = `./uploads/image-${Date.now() + '.' + extension}`
+
+      const product = new Product({
+        title: req.body.title,
+        category: req.body.category,
+        brand: req.body.brand,
+        color: req.body.color,
+        description: req.body.description,
+        modelNum: req.body.modelNum,
+        price: req.body.price,
+        length: req.body.length,
+        width: req.body.width,
+        height: req.body.height,
+        weight: req.body.weight,
+        quantity: req.body.quantity,
+        published: false,
+        image: path
+      });
+      try {
+
+
+        await product.save()
+        let allProducts = await Product.find().exec()
+
+        //save and resize pic
+        await sharp(req.file.buffer).resize(300, 300).toFile(path);
+      
+        res.status(200).json(allProducts)
+      } catch (error) {
+
+        res.status(500).json({ message: error });
       }
-    
-    ]
-    
+    }
+
+    else {
+      const product = new Product({
+        title: req.body.title,
+        category: req.body.category,
+        brand: req.body.brand,
+        color: req.body.color,
+        description: req.body.description,
+        modelNum: req.body.modelNum,
+        price: req.body.price,
+        length: req.body.length,
+        width: req.body.width,
+        height: req.body.height,
+        weight: req.body.weight,
+        quantity: req.body.quantity,
+        published: false
+
+      });
+      try {
+        await product.save()
+        let allProducts = await Product.find().exec()
+        res.status(200).json(allProducts)
+      } catch (error) {
+        res.status(500).json({ message: error });
+      }
+    }
+
+
+  }
+
+]
+
 // get all products
 
 exports.all_products_get = asyncHandler(async (req, res) => {
@@ -200,7 +239,7 @@ exports.delete_product = asyncHandler(async (req, res) => {
 
     //delete pic file
     if (picProduct.image) {
-      fs.unlink("./uploads/" + picProduct.image, (err) => {
+      fs.unlink(picProduct.image, (err) => {
         if (err) {
           throw err;
         }
@@ -299,20 +338,20 @@ exports.edit_product = asyncHandler(async (req, res) => {
   const product = new Product({
 
     title: req.body.title,
-      category: req.body.category,
-      brand: req.body.brand,
-      color: req.body.color,
-      description: req.body.description,
-      modelNum: req.body.modelNum,
-      price: req.body.price,
-      length: req.body.length,
-      width: req.body.width,
-      height: req.body.height,
-      weight: req.body.weight,
-      quantity: req.body.quantity,
-      published: req.body.published,
-      _id: req.params.productId
-    
+    category: req.body.category,
+    brand: req.body.brand,
+    color: req.body.color,
+    description: req.body.description,
+    modelNum: req.body.modelNum,
+    price: req.body.price,
+    length: req.body.length,
+    width: req.body.width,
+    height: req.body.height,
+    weight: req.body.weight,
+    quantity: req.body.quantity,
+    published: req.body.published,
+    _id: req.params.productId
+
   });
 
   try {
@@ -335,7 +374,7 @@ exports.image_delete = asyncHandler(async (req, res) => {
 
     //delete pic file
     if (picProduct.image) {
-      fs.unlink("./uploads/" + picProduct.image, (err) => {
+      fs.unlink(picProduct.image, (err) => {
         if (err) {
           throw err;
         }
@@ -363,17 +402,22 @@ exports.image_delete = asyncHandler(async (req, res) => {
 exports.image_post = [
 
   // Handle single file upload with field name "image"
-  upload.single("image"),
-
+  //upload.single("image"),
+  imageUploader.single('image'),
 
   async function (req, res) {
 
 
     let productData = await Product.findById(req.params.productId);
 
+    // path: where to store resized photo
+    let extArray = req.file.mimetype.split("/");
+    let extension = extArray[extArray.length - 1];
+    const path = `./uploads/image-${Date.now() + '.' + extension}`
+
 
     const product = new Product({
-      
+
       title: productData.title,
       category: productData.category,
       brand: productData.brand,
@@ -388,10 +432,13 @@ exports.image_post = [
       quantity: productData.quantity,
       published: productData.published,
       _id: req.params.productId,
-      image: req.file.filename
+      image: path
     });
 
     try {
+      //save and resize pic
+      await sharp(req.file.buffer).resize(300, 300).toFile(path);
+
       await Product.findByIdAndUpdate(req.params.productId, product, {});
       let allProducts = await Product.find().exec()
       res.status(200).json(allProducts)
