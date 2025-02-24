fed347f3feb881aa3a43fe9706839b9993bc6432
craigmarc <cmarcinkiewicz2000@yahoo.com>
2025-02-24 10:20:11 -0500

    restricted cors

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["https://shopping-cart-topaz-sigma.vercel.app", "http://localhost:5173"],
+    origin: ["https://shopping-cart-topaz-sigma.vercel.app"],
     credentials: true,
   })
 );
